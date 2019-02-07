8cf5c9777e28b1da6097f4b125414ef0d2874c04
Jorge Mercado Ferreño <jorgemercadof@hotmail.com>
2019-02-07 14:48:01 +0100

    master

--- EjercicioLinQtoJSon/Program.cs
+++ EjercicioLinQtoJSon/Program.cs
@@ -9,38 +9,182 @@ using System.Threading.Tasks;
 
 namespace EjercicioLinQtoJSon
 {
+
+    #region Clases API
+    class Posts
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    class Comments
+    {
+        public int PostId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Body { get; set; }
+    }
+
+    class Albums
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+    class Photos
+    {
+        public int AlbumId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+
+    class Todos
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Completed { get; set; }
+    }
+
+    class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public Adress Adress { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public Company Company { get; set; }
+    }
+
+    class Geo
+    {
+        public float Lat { get; set; }
+        public float Lng { get; set; }
+    }
+
+    class Adress
+    {
+        public string Street { get; set; }
+        public string Suite { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public Geo Geo { get; set; }
+    }
+
+    class Company
+    {
+        public string Name { get; set; }
+        public string CatchPhrase { get; set; }
+        public string Bs { get; set; }
+    }
+    #endregion
+
+    #region Metodos y Main
     class Program
     {
-        static async void leeMuseosVisitas()
+        //Ver cuales son los comentarios que hay para un post determinado.
+        static async void Ejercicio1(int postId)
         {
             using (var Clientes = new HttpClient())
             {
-                string url = @"https://museowebapp.azurewebsites.net/api/MuseosAPI";
-                var uri = new Uri(url);
+                string urlComments = @"http://jsonplaceholder.typicode.com/comments";
+                var uriComments = new Uri(urlComments);
 
-                var response = await Clientes.GetAsync(uri);
-                if (response.IsSuccessStatusCode)
+                var responseComments = await Clientes.GetAsync(uriComments);
+                if (responseComments.IsSuccessStatusCode)
                 {
-                    var content = await response.Content.ReadAsStringAsync();
-                    JArray Array = JArray.Parse(content);
-                    var museosVisitados = from x in Array
-                                          where (string)x["visita"] == "S"
-                                          select x;
+                    var contentComments = await responseComments.Content.ReadAsStringAsync();
+                    JArray ContenidoComments = JArray.Parse(contentComments);
+                    var comentariosPostId = from x in ContenidoComments
+                                            where (int)x["postId"] == postId
+                                            select x;
 
-                    foreach (JToken mus in museosVisitados)
+                    foreach (JToken comentario in comentariosPostId)
                     {
-                        Console.WriteLine(mus.ToString());
+                        Console.WriteLine("Titulo: " + comentario["name"].ToString() + " [" + comentario["email"].ToString() + "] " + "\n" + comentario["body"] + "\n\n");
                     }
                 }
-
             }
+        }
+
+        //ver cuales son los albums que hay y la cantidad de fotos que contiene cada uno de ellos
+        static async void Ejercicio2()
+        {
+            using (var Clientes = new HttpClient())
+            {
+                string urlAlbums = @"http://jsonplaceholder.typicode.com/albums";
+                string urlPhotos = @"http://jsonplaceholder.typicode.com/photos";
+                var uriAlbums = new Uri(urlAlbums);
+                var uriPhotos = new Uri(urlPhotos);
+
+                var responseAlbums = await Clientes.GetAsync(uriAlbums);
+                var responsePhotos = await Clientes.GetAsync(uriPhotos);
+                if (responseAlbums.IsSuccessStatusCode && responsePhotos.IsSuccessStatusCode)
+                {
+                    var contentAlbums = await responseAlbums.Content.ReadAsStringAsync();
+                    var contentPhotos = await responsePhotos.Content.ReadAsStringAsync();
+                    JArray ContenidoAlbums = JArray.Parse(contentAlbums);
+                    JArray ContenidoPhotos = JArray.Parse(contentPhotos);
+                    var albumes = from x in ContenidoAlbums
+                                  select x;
+
+                    foreach (JToken album in albumes)
+                    {
+                        var photos = from y in ContenidoPhotos
+                                     where (int)y["albumId"] == ((int)album["id"])
+                                     select y;
+                        Console.WriteLine("El album: " + album["title"].ToString() + " contiene: " + photos.Count() + "\n\n");
+                    }
+                }
+            }
+        }
+
+        //ver cuales son los nombres de los usuarios ordenados descendentemente.
+        static async void Ejercicio3()
+        {
+            using (var Clientes = new HttpClient())
+            {
+                string urlUsers = @"http://jsonplaceholder.typicode.com/users";
+                var uriUsers = new Uri(urlUsers);
+
+                var responseUsers = await Clientes.GetAsync(uriUsers);
+                if (responseUsers.IsSuccessStatusCode)
+                {
+                    var contentUsers = await responseUsers.Content.ReadAsStringAsync();
+                    JArray ContenidoUsers = JArray.Parse(contentUsers);
+                    var users = from x in ContenidoUsers
+                                orderby x["name"] descending
+                                select x;
+
+                    foreach(var user in users)
+                    {
+                        Console.WriteLine(user["name"].ToString() + "\n");
+                    }
+                }
+            }
+        }
+
+        //Ver que cantidad de comentarios totales ha recibido un usuario.
+        static async void Ejercicio4(int idUsers)
+        {
+
         }
 
         static void Main(string[] args)
         {
-            //leeMuseos();
-            leeMuseosVisitas();
+            //Ejercicio1(2);
+            //Ejercicio2();
+            Ejercicio3();
             Console.ReadLine();
         }
     }
+    #endregion
 }
